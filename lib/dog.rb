4006b98c69032252351f895f6517441f908eb2da
Douglas Obara <douglasobara97@gmail.com>
2022-08-20 14:21:54 +0300

    complete assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,13 @@
+# name=
+# defines an instance variable @this_dogs_name
+# name
+# retrieves the value of an instance variable @this_dogs_name
 class Dog
-
+  def name=(dog_name)
+    @this_dogs_name = dog_name
+  end
+ 
+  def name
+    @this_dogs_name
+  end
 end
